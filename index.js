c4502dde75045bf67bc532112fedc78fe0cbe83d
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-28 17:41:19 +0500

    set up express server with body and cookie parsers; add pug view engine and create initial index.pug

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to Real time chat app");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
