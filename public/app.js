9e4dfae293f0f4ed6b11fbb333f5ced3dc7d5794
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-29 15:04:25 +0500

    add user block generation and online users handling with socket.io

---
+++ public/app.js
@@ -0,0 +1,37 @@
+const socket = io("/");
+
+function usersBlockGenerator(onlineUsers) {
+  const onlineUsersParent = document.querySelector("#online-users");
+  onlineUsersParent.innerHTML = "";
+  for (const user of onlineUsers) {
+    const nameNode = genUsername(user.username);
+    const outerNode = genUserBlock(user.userId);
+    outerNode.appendChild(nameNode);
+    onlineUsersParent.appendChild(outerNode);
+  }
+}
+const genUserBlock = (userId) => {
+  const userBlock = document.createElement("div");
+  userBlock.classList.add(
+    "p-2",
+    "bg-white",
+    "w-full",
+    "rounded-md",
+    "cursor-pointer"
+  );
+  userBlock.id = userId;
+  return userBlock;
+};
+
+const genUsername = (username) => {
+  const userName = document.createElement("h2");
+  userName.classList.add("text-base", "mx-auto");
+  userName.innerText = username;
+  return userName;
+};
+
+socket.on("get_online_users", (usersOnline) => {
+  usersOnline = usersOnline.filter((user) => user.id !== socket.id);
+  console.log(usersOnline);
+  usersBlockGenerator(usersOnline);
+});
