aa818151bdf94aa4ec1eceb559b210bb5c2e88b5
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-29 22:16:12 +0500

    added functionality to update receiver and display active chat user

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
 const socket = io("/");
 
+let receiver = "baat-cheet";
+
+const onlineUsersParent = document.querySelector("#online-users");
+const chattingWith = document.querySelector("#chatting-with");
 function usersBlockGenerator(onlineUsers) {
-  const onlineUsersParent = document.querySelector("#online-users");
   onlineUsersParent.innerHTML = "";
   for (const user of onlineUsers) {
     const nameNode = genUsername(user.username);
     const outerNode = genUserBlock(user.userId);
     outerNode.appendChild(nameNode);
+    outerNode.addEventListener("click", () => {
+      receiver = user.userId;
+      chattingWith.innerText = `You are chatting with ${user.username.toLowerCase()}`;
+    });
     onlineUsersParent.appendChild(outerNode);
   }
 }
@@ -30,9 +37,40 @@ const genUsername = (username) => {
   return userName;
 };
 
+const messageBox = document.querySelector("#all-messages");
+const messageForm = document.querySelector("#message-form");
+const messageInput = messageForm.querySelector("textarea");
+
+const genMessage = (message, pos) => {
+  const newMessage = document.createElement("div");
+  newMessage.classList.add(
+    "bg-white",
+    "text-sm",
+    "text-black",
+    "p-1",
+    "rounded-sm",
+    "max-w-[50%]",
+    "w-fit",
+    "max-h-18",
+    "h-fit",
+    `self-${pos}`
+  );
+  newMessage.innerText = message.trim();
+  messageBox.appendChild(newMessage);
+};
+
+messageForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  genMessage(messageInput.value.trim(), "end");
+  socket.emit("private_message", {
+    toUserId: receiver,
+    message: messageInput.value.trim()
+  });
+  messageInput.value = "";
+});
+
 socket.on("get_online_users", (usersOnline) => {
   usersOnline = usersOnline.filter((user) => user.id !== socket.id);
-  console.log(usersOnline);
   usersBlockGenerator(usersOnline);
 });
 
@@ -42,4 +80,8 @@ socket.on("already_online", () => {
     alertBox.classList.remove("hidden");
     alertBox.classList.add("fixed");
   });
+});
+
+socket.on("private_message", (data) => {
+  receiver === data.fromUserId ? genMessage(data.message, "start") : "";
 });
